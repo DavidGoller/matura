1e463311667fc15058bb37f9d848e8aad8bd1128
DavidGoller <davidgoller21@gmail.com>
2023-11-21 09:06:38 +0100

    Tailwind?

--- Tailwind-starter/dist/index.php
+++ Tailwind-starter/dist/index.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../css/style.css" rel="stylesheet">
+    <link href="output.css" rel="stylesheet">
 </head>
 <body>
 <h1 class="bg-amber-500">
-    Hallo World
+    Hello world!
 </h1>
 </body>
 </html>
