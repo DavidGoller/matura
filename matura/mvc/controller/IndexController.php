7006f588a6e294cdf0775945fa697a734174178b
DavidGoller <davidgoller21@gmail.com>
2024-01-30 09:16:05 +0100

    registration2MVC

--- matura/mvc/controller/IndexController.php
+++ matura/mvc/controller/IndexController.php
@@ -1,16 +1,36 @@
 <?php
 
-class IndexController extends AbstractBase {
+class IndexController extends AbstractBase
+{
 
     // Alle Seminartermine auslesen
-    public function home() {
+    public function home()
+    {
+    }
+    public function headerbg()
+    {
+    }
+    public function login()
+    {
+    }
+
+    public function registration()
+    {
+        if ($_POST) {
+            $password1 = $_POST['password'];
+            $password2 = $_POST['confirmpassword'];
+            if ($password1 == $password2) {
+                redirect("index.php?aktion=registration2");
+            }
+        }
+    }
+    public function registration2(){
 
     }
-    public function headerbg() {
+    public function registration3(){
 
     }
-    public function login(){
+    public function registration4(){
         
     }
-
 }
